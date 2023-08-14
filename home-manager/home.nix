df31bbe80cf2b5b1732922f102ab945b6e1ecaa9
Ben Coleman <benjamin.coleman@me.com>
2023-08-14 19:59:38 +0100

    Configuring zsh

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -109,4 +109,19 @@
       vim-nix
     ];
   };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    enableAutosuggestions = true;
+    shellAliases = {
+      ll = "ls -la";
+      update = "home-manager switch";
+    };
+    oh-my-zsh = {
+      enable = true;
+      plugins = ["sudo" "git"];
+      theme = "robbyrussell";
+    };
+  };
 }
