65fe3cd334d6a2cafc56df6bfa4b5920fcafd4c4
Ben Coleman <benjamin.coleman@me.com>
2023-08-15 10:56:15 +0100

    Added audio and mem to pb

--- home-manager/polybar.nix
+++ home-manager/polybar.nix
@@ -26,7 +26,7 @@
         radius = 0;
         modules-left = "bspwm";
         modules-center = "xwindow";
-        modules-right = "cpu sep date";
+        modules-right = "alsa sep memory sep cpu sep date";
         wm-restack = "bspwm";
 
         background = "\${colors.background}";
@@ -81,7 +81,41 @@
         interval = 2;
         format-prefix-foreground = "\${colors.foreground-alt}";
         format-underline = "\${colors.secondary}";
-        label = "﬙ %percentage:2%%";
+        label = "%percentage:2%%";
+      };
+      "module/memory" = {
+        type = "internal/memory";
+        interval = 2;
+        format-prefix-foreground = "\${colors.foreground-alt}";
+        format-underline = "#4bffdc";
+        label = "%gb_used%";
+      };
+      "module/alsa" = {
+        type = "internal/alsa";
+
+        format-volume = "<label-volume> <bar-volume>";
+        label-volume = " %percentage%%";
+        label-volume-foreground = "\${root.foreground}";
+
+        label-muted = " ﱝ muted";
+        label-muted-foreground = "#666";
+
+        bar-volume-width = 10;
+        bar-volume-foreground-0 = "#55aa55";
+        bar-volume-foreground-1 = "#55aa55";
+        bar-volume-foreground-2 = "#55aa55";
+        bar-volume-foreground-3 = "#55aa55";
+        bar-volume-foreground-4 = "#55aa55";
+        bar-volume-foreground-5 = "#f5a70a";
+        bar-volume-foreground-6 = "#ff5555";
+        bar-volume-gradient = false;
+        bar-volume-indicator = "|";
+        bar-volume-indicator-font = 2;
+        bar-volume-fill = "─";
+        bar-volume-fill-font = 2;
+        bar-volume-empty = "─";
+        bar-volume-empty-font = 2;
+        bar-volume-empty-foreground = "\${colors.foreground-alt}";
       };
       "module/sep" = {
         type = "custom/text";
