c6d2d581519270a97c7d1e581fc1479c02350a58
Ben Coleman <benjamin.coleman@me.com>
2024-12-19 17:02:52 +0000

    init hyprland

---
+++ home/common/waybar.nix
@@ -0,0 +1,254 @@
+# env GTK_DEBUG=interactive waybar
+{ pkgs, ... }:
+{
+  programs.waybar = {
+    enable = true;
+    package = pkgs.unstable.waybar;
+    style = ''
+    /* Gruvbox Palette */
+    @define-color foreground #ebdbb2;
+    @define-color background #1d2021;
+    @define-color alt_background #282828;
+    @define-color accent #689d6a;
+
+    @define-color black #282828;
+    @define-color red #cc241d;
+    @define-color green #98971a;
+    @define-color blue #7daea3;
+    @define-color yellow #d79921;
+    @define-color magenta #b16286;
+    @define-color cyan #689d6a;
+    @define-color white #a89984;
+
+    @define-color alt_black #3c3836;
+    @define-color alt_red #fb4934;
+    @define-color alt_green #b8bb26;
+    @define-color alt_blue #7daea3;
+    @define-color alt_yellow #fabd2f;
+    @define-color alt_magenta #d3869b;
+    @define-color alt_cyan #8ec07c;
+    @define-color alt_white #ebdbb2;
+
+    * {
+    
+      border: none;
+      font-family: FiraCode Nerd Font;
+      font-size: 14px;
+    }
+
+    window#waybar {
+      /* background-color: rgba(18, 21, 29, 0.98); */
+      background-color: @background;
+      /* background-color: rgba(0, 0, 0, 0); */
+      border-radius: 0px;
+      color: @foreground;
+      opacity: 1;
+      transition-property: background-color;
+      transition-duration: .5s;
+      margin-bottom: -7px;
+    }
+
+    window#waybar.hidden {
+      opacity: 0.2;
+    }
+
+    window#hyprland-window {
+      background-color: @background;
+    }
+
+    #workspaces,
+    #mode,
+    #window,
+    #cpu,
+    #memory,
+    #temperature,
+    #custom-media,
+    #custom-powermenu,
+    #custom-fans,
+    #custom-wmname,
+    #clock,
+    #idle_inhibitor,
+    #language,
+    #pulseaudio,
+    #backlight,
+    #battery,
+    #network,
+    #tray {
+      background-color: @alt_background;
+      padding: 0 10px;
+      margin: 5px 2px 5px 2px;
+      border: 1px solid rgba(0, 0, 0, 0);
+      border-radius: 6px;
+      background-clip: padding-box;
+    }
+
+    #workspaces button {
+      background-color: @alt_background;
+      padding: 0 5px;
+      min-width: 20px;
+      color: @foreground;
+    }
+
+    #workspaces button:hover {
+      background-color: rgba(0, 0, 0, 0)
+    }
+
+    #workspaces button.active {
+      color: @accent;
+    }
+
+    #workspaces button.urgent {
+      color: @red;
+    }
+
+    #cpu {
+      padding: 0 10px;
+      color: @alt_cyan;
+    }
+
+    #memory {
+      padding: 0 10px;
+      color: @alt_cyan;
+    }
+
+    #temperature {
+      padding: 0 10px;
+      color: @blue;
+    }
+
+    #temperature.critical {
+      background-color: @red;
+      padding: 0 10px;
+      color: @background;
+    }
+
+    #custom-media {
+      color: #c678dd;
+      padding: 0 10px;
+      color: @background;
+    }
+
+    #custom-fans {
+      padding: 0 10px;
+      color: @blue;
+    }
+
+    #clock {
+      padding: 0 10px;
+      color: @blue;
+    }
+
+    #idle_inhibitor {
+      padding: 0 10px;
+      color: @foreground;
+    }
+
+    #language {
+      padding: 0 10px;
+      color: @blue;
+    }
+
+    #pulseaudio {
+      padding: 0 10px;
+      color: @yellow;
+    }
+
+    #pulseaudio.muted {
+      padding: 0 10px;
+      background-color: @red;
+      color: @background;
+    }
+
+    #backlight {
+      padding: 0 10px;
+      color: @yellow;
+    }
+
+    #battery {
+      padding: 0 10px;
+      color: @alt_green;
+    }
+
+    #battery.charging,
+    #battery.plugged {
+      padding: 0 10px;
+      background-color: @alt_green;
+      color: @background;
+    }
+
+    @keyframes blink {
+      to {
+        background-color: @background;
+        color: @red;
+      }
+    }
+
+    #battery.critical:not(.charging) {
+      padding: 0 10px;
+      background-color: @red;
+      color: @background;
+      animation-name: blink;
+      animation-duration: 0.5s;
+      animation-timing-function: linear;
+      animation-iteration-count: infinite;
+      animation-direction: alternate;
+    }
+
+    #network {
+      padding: 0 10px;
+      color: @blue;
+    }
+
+    #custom-wmname {
+      color: @accent;
+      background-color: @background;
+      font-size: 25px;
+      margin: 1px;
+      padding: 0px 0px 0px 5px;
+    }
+
+    #network.disconnected {
+      padding: 0 10px;
+      background-color: @red;
+      color: @background;
+    }
+
+    #custom-powermenu {
+      background-color: @red;
+      color: @background;
+      font-size: 15px;
+      padding-right: 6px;
+      padding-left: 11px;
+      margin: 5px;
+    }
+    '';
+    settings = {
+      mainBar = {
+        margin = "0px 0px 0px 0px";
+        layer = "top";
+        output = "eDP-1";
+        position = "top";
+        modules-left = ["hyprland/workspaces"];
+        modules-center = ["hyprland/window"];
+        modules-right = ["battery" "cpu" "memory" "pulseaudio" "tray" "clock"];
+      };
+      "hyprland/workspaces" = {
+        active-only = "false";
+        on-click = "activate";
+      };
+      "battery" = {
+        states = {
+          warning = "30";
+          critical = "15";
+        };
+         format = "{icon}{capacity}%";
+         tooltip-format = "{timeTo} {capacity}%";
+         format-charging = "󰂄{capacity}%";
+         format-plugged = " ";
+         format-alt = "{time} {icon}";
+         format-icons = ["󰁺  " "󰁼  " "󰁾  " "󰂀  " "󰂂  "];
+      };
+    };
+    systemd.enable = true;
+  };
+}
---
+++ home/francesca/hyprland.nix
@@ -0,0 +1,104 @@
+{ pkgs, ...}:
+{
+  imports = [
+    # ../common/waybar.nix
+  ];
+  home.file.".config/hypr/hyprpaper.conf".source = ../etc/hyprpaper.conf;
+  wayland.windowManager.hyprland = {
+    enable = true;
+    extraConfig = ''
+      exec-once=${pkgs.hyprpaper}/bin/hyprpaper
+    '';
+    settings = {
+      monitor = ",highres,auto,1";
+      general = {
+        gaps_out = 10;
+      };
+      decoration = {
+        rounding = 2;
+    
+        blur = {
+          enabled = true;
+          size = 3;
+          passes = 1;
+        };
+
+        drop_shadow = "yes";
+        shadow_range = 4;
+        shadow_render_power = 3;
+        "col.shadow" = "rgba(1a1a1aee)";
+      };
+      "$mainMod" = "SUPER";
+      "$secondMod" = "SUPER + SHIFT";
+      workspace = [
+        "1,monitor:eDP-1,persistent:true"
+        "2,monitor:eDP-1,persistent:true"
+        "3,monitor:eDP-1,persistent:true"
+        "4,monitor:eDP-1,persistent:true"
+        "5,monitor:eDP-1,persistent:true"
+        "6,monitor:eDP-1,persistent:true"
+        "7,monitor:eDP-1,persistent:true"
+        "8,monitor:eDP-1,persistent:true"
+      ];
+      animation = [
+        "windowsMove,0" # Don't animate moving windows - it's annoying
+        "windowsIn,1,1,default" # Start window creation faster
+        "workspaces,1,2,default"
+      ];
+      bind = [
+        "$mainMod, Return, exec, kitty"
+        "$secondMod, Return, exec, firefox"
+        "$mainMod, Space, exec, rofi -show drun" #WOFI?
+        "$mainMod, Q, killactive, "
+        "SUPER + ALT, Q, exit, "
+        "$mainMod, S, togglefloating"
+        "$mainMod, F, fullscreen"
+        "$secondMod, F, fakefullscreen"
+        "$mainMod, T, togglesplit, # dwindle"
+
+        "$mainMod, left, movefocus, l"
+        "$mainMod, right, movefocus, r"
+        "$mainMod, up, movefocus, u"
+        "$mainMod, down, movefocus, d"
+
+        "CTRL + SHIFT, left, layoutmsg, preselect l" # not working...
+        "CTRL + SHIFT, right, layoutmsg, preselect r"
+        "CTRL + SHIFT, up, layoutmsg, preselect u"
+        "CTRL + SHIFT, down, layoutmsg, preselect d"
+      
+        "$mainMod, 1, workspace, 1"
+        "$mainMod, 2, workspace, 2"
+        "$mainMod, 3, workspace, 3"
+        "$mainMod, 4, workspace, 4"
+        "$mainMod, 5, workspace, 5"
+        "$mainMod, 6, workspace, 6"
+        "$mainMod, 7, workspace, 7"
+        "$mainMod, 8, workspace, 8"
+        "$mainMod, 9, workspace, 9"
+        "$mainMod, 0, workspace, 10"
+      
+        "$mainMod SHIFT, 1, movetoworkspacesilent, 1"
+        "$mainMod SHIFT, 2, movetoworkspacesilent, 2"
+        "$mainMod SHIFT, 3, movetoworkspacesilent, 3"
+        "$mainMod SHIFT, 4, movetoworkspacesilent, 4"
+        "$mainMod SHIFT, 5, movetoworkspacesilent, 5"
+        "$mainMod SHIFT, 6, movetoworkspacesilent, 6"
+        "$mainMod SHIFT, 7, movetoworkspacesilent, 7"
+        "$mainMod SHIFT, 8, movetoworkspacesilent, 8"
+        "$mainMod SHIFT, 9, movetoworkspacesilent, 9"
+        "$mainMod SHIFT, 0, movetoworkspacesilent, 10"
+
+        "$secondMod, left, movewindow, l"
+        "$secondMod, right, movewindow, r"
+        "$secondMod, up, movewindow, u"
+        "$secondMod, down, movewindow, d"
+      ]; 
+
+      bindm = [
+        # Move/resize windows with mainMod + LMB/RMB and dragging
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
+      ];
+    };
+  };
+}
